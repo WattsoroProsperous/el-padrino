33604e0fc7eafa37e7d00d437570c464f73fe2c5
WattsoroProsperous <watantcha724@gmail.com>
2025-12-05 23:28:08 +0000

    El padrinos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // Décommentez et modifiez si votre repo n'est pas à la racine
+  // basePath: "/nom-du-repo",
+  // assetPrefix: "/nom-du-repo/",
 };
 
 export default nextConfig;
